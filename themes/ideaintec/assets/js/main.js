a5a69c0fcbeefa18245d09bc05526a2edb3ed34f
Rannojit <cserannojitsarkar22@gmail.com>
2021-08-26 17:24:50 +0600

    .

---
+++ themes/ideaintec/assets/js/main.js
@@ -0,0 +1,461 @@
+(function($) {
+
+/*Google Map Style*/
+var CustomMapStyles  = [{"featureType":"water","elementType":"geometry","stylers":[{"color":"#e9e9e9"},{"lightness":17}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":20}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffffff"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#ffffff"},{"lightness":29},{"weight":.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":16}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":21}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#dedede"},{"lightness":21}]},{"elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#ffffff"},{"lightness":16}]},{"elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#333333"},{"lightness":40}]},{"elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#f2f2f2"},{"lightness":19}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#fefefe"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#fefefe"},{"lightness":17},{"weight":1.2}]}]
+
+$(window).load(function() {
+    $('.home').addClass('banner-effect');
+});
+
+
+var windowWidth = $(window).width();
+$('.navbar-toggle').on('click', function(){
+	$('#mobile-nav').slideToggle(300);
+});
+	
+if($("ul.slick-dots li").length == 1){
+   $("ul.slick-dots").hide();
+}
+//matchHeightCol
+if($('.mHc').length){
+  $('.mHc').matchHeight();
+};
+if($('.mHc1').length){
+  $('.mHc1').matchHeight();
+};
+if($('.mHc2').length){
+  $('.mHc2').matchHeight();
+};
+if($('.mHc3').length){
+  $('.mHc3').matchHeight();
+};
+if($('.mHc4').length){
+  $('.mHc4').matchHeight();
+};
+if($('.mHc5').length){
+  $('.mHc5').matchHeight();
+};
+if($('.mHc6').length){
+  $('.mHc6').matchHeight();
+};
+$(window).load(function() {
+//matchHeightCol
+  if($('.mHc').length){
+    $('.mHc').matchHeight();
+  };
+  if($('.mHc1').length){
+    $('.mHc1').matchHeight();
+  };
+  if($('.mHc2').length){
+    $('.mHc2').matchHeight();
+  };
+  if($('.mHc3').length){
+    $('.mHc3').matchHeight();
+  };
+  if($('.mHc4').length){
+    $('.mHc4').matchHeight();
+  };
+  if($('.mHc5').length){
+    $('.mHc5').matchHeight();
+  };
+  if($('.mHc6').length){
+    $('.mHc6').matchHeight();
+  };
+});
+
+//$('[data-toggle="tooltip"]').tooltip();
+
+//banner animation
+$(window).scroll(function() {
+  var scroll = $(window).scrollTop();
+  $('.page-banner-bg').css({
+    '-webkit-transform' : 'scale(' + (1 + scroll/2000) + ')',
+    '-moz-transform'    : 'scale(' + (1 + scroll/2000) + ')',
+    '-ms-transform'     : 'scale(' + (1 + scroll/2000) + ')',
+    '-o-transform'      : 'scale(' + (1 + scroll/2000) + ')',
+    'transform'         : 'scale(' + (1 + scroll/2000) + ')'
+  });
+});
+
+
+if($('.fancybox').length){
+$('.fancybox').fancybox({
+    //openEffect  : 'none',
+    //closeEffect : 'none'
+  });
+
+}
+
+
+/**
+Responsive on 767px
+*/
+
+// if (windowWidth <= 767) {
+  $('.toggle-btn').on('click', function(){
+    $(this).toggleClass('menu-expend');
+    $('.toggle-bar ul').slideToggle(500);
+  });
+
+
+// }
+
+
+
+// http://codepen.io/norman_pixelkings/pen/NNbqgG
+// https://stackoverflow.com/questions/38686650/slick-slides-on-pagination-hover
+
+
+/**
+Slick slider
+*/
+if( $('.responsive-slider').length ){
+    $('.responsive-slider').slick({
+      dots: true,
+      infinite: false,
+      autoplay: true,
+      autoplaySpeed: 4000,
+      speed: 700,
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
+    });
+}
+
+
+var swiper = new Swiper('.catagorySlider', {
+    slidesPerView: 1,
+    loop: true,
+    navigation: {
+      nextEl: '.catagorySlider-arrows .swiper-button-next',
+      prevEl: '.catagorySlider-arrows .swiper-button-prev',
+    },
+    breakpoints: {
+       639: {
+        slidesPerView: 2,
+        spaceBetween: 0,
+      },
+      991: {
+        slidesPerView: 3,
+        spaceBetween: 0,
+      },
+      1199: {
+        loop: false,
+        slidesPerView: 4,
+        spaceBetween: 0,
+      },
+      1920: {
+        loop: false,
+        slidesPerView: 4,
+        spaceBetween: 0,
+      },
+    }
+  });
+
+if( $('#mapID').length ){
+var latitude = $('#mapID').data('latitude');
+var longitude = $('#mapID').data('longitude');
+
+var myCenter= new google.maps.LatLng(latitude,  longitude);
+function initialize(){
+    var mapProp = {
+      center:myCenter,
+      mapTypeControl:true,
+      scrollwheel: false,
+      zoomControl: true,
+      disableDefaultUI: true,
+      zoom:7,
+      streetViewControl: false,
+      rotateControl: true,
+      mapTypeId:google.maps.MapTypeId.ROADMAP,
+      styles: CustomMapStyles
+      };
+
+    var map= new google.maps.Map(document.getElementById('mapID'),mapProp);
+    var marker= new google.maps.Marker({
+      position:myCenter,
+        //icon:'map-marker.png'
+      });
+    marker.setMap(map);
+}
+google.maps.event.addDomListener(window, 'load', initialize);
+
+}
+
+
+
+/* BS form Validator*/
+(function() {
+  'use strict';
+  window.addEventListener('load', function() {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.getElementsByClassName('needs-validation');
+    // Loop over them and prevent submission
+    var validation = Array.prototype.filter.call(forms, function(form) {
+      form.addEventListener('submit', function(event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+  }, false);
+})();
+
+    new WOW().init();
+
+
+
+
+/* after code */
+//fixed header
+$(window).scroll(function() { 
+    var scroll = $(window).scrollTop();   
+    if (scroll >= 5) {
+        $('.hdr-top-bar').addClass('fixed-hdr');
+    } else {
+        $('.hdr-top-bar').removeClass('fixed-hdr');
+    }  
+});
+
+
+
+/**
+--------------------
+Responsive on 767px
+---------------------
+*/
+var windowWidth = $(window).width();
+if (windowWidth <= 767) {
+
+  $('.opener-inner').on('click', function(){
+    $(this).toggleClass('menu-expend');
+    $('nav.main-nav > ul').slideToggle(500);
+  });
+
+}
+
+
+/**
+------------------
+Isotope
+------------------
+*/ 
+if($('#filter-menu').length){
+  // init Isotope
+  var $container = $('#isotope').isotope({
+    itemSelector: '.item',
+    layoutMode: 'fitRows',
+
+  });
+  // bind filter button click
+  $('#filter-menu').on( 'click', 'a', function(e) {
+    e.preventDefault();
+    $('#filter-menu a').removeClass('is-checked');
+    $(this).addClass('is-checked');
+    var filterValue = $( this ).attr('data-filter');
+    $container.isotope({ filter: filterValue });
+  });
+}
+
+
+
+
+/**
+------------------
+Nivo slider
+------------------
+*/
+if($('#slider').length){
+    $('#slider').nivoSlider({
+        controlNavThumbs: true
+    });
+}
+
+/**
+------------------
+Owl carouserl 1 slider
+------------------
+*/
+if($('#slider').length){
+   $('#slider').owlCarousel({
+    singleItem : true,
+    pagination: true,
+    autoPlay : false,//8000,
+    addClassActive: true,
+    stopOnHover : true,   
+    afterMove: function(el){
+        $('.owl-item.active .slide-heading-caption').addClass('animated animated-d15 fadeInDown');
+
+ 
+    },
+    beforeMove : function (el) {
+        $('.owl-item.active .slide-heading-caption').removeClass('animated animated-d15 fadeInDown ');
+ 
+    },
+    afterAction : function (el){
+        $('.owl-item.active .slide-heading-caption').addClass('animated animated-d15 fadeInDown');
+    },
+
+    });
+}
+
+
+// http://codepen.io/norman_pixelkings/pen/NNbqgG
+// https://stackoverflow.com/questions/38686650/slick-slides-on-pagination-hover
+/**
+Slick slider
+*/
+if( $('responsive-slider').length ){
+    $('.responsive-slider').slick({
+      dots: true,
+      infinite: false,
+      speed: 300,
+      slidesToShow: 4,
+      slidesToScroll: 4,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
+    });
+}
+
+
+
+/**
+------------------
+To top
+------------------
+*/
+$(window).scroll(function() {
+    if($(this).scrollTop() != 0) {
+        $('#toTop').fadeIn();    
+    } else {
+        $('#toTop').fadeOut();
+    }
+});
+
+$('#toTop').click(function() {
+    $('body,html').animate({scrollTop:0},800);
+}); 
+
+
+
+//Goole map
+if( $('#mapID').length ){
+var styles = [{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#0c0b0b"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2f2f2"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45}]},{"featureType":"road","elementType":"labels.text.fill","stylers":[{"color":"#090909"}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#d4e4eb"},{"visibility":"on"}]},{"featureType":"water","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#fef7f7"}]},{"featureType":"water","elementType":"labels.text.fill","stylers":[{"color":"#9b7f7f"}]},{"featureType":"water","elementType":"labels.text.stroke","stylers":[{"color":"#fef7f7"}]}];
+var latitude = $('#mapID').data('latitude');
+var longitude = $('#mapID').data('longitude');
+
+var myCenter= new google.maps.LatLng(latitude,  longitude);
+
+function initialize(){
+    var mapProp = {
+      center:myCenter,
+
+      mapTypeControl:true,
+      scrollwheel: false,
+
+      zoomControl: true,
+      disableDefaultUI: true,
+      zoom:7,
+      streetViewControl: false,
+      rotateControl: true,
+      mapTypeId:google.maps.MapTypeId.ROADMAP,
+      styles : styles,
+      };
+
+    var map= new google.maps.Map(document.getElementById('mapID'),mapProp);
+
+    var marker= new google.maps.Marker({
+      position:myCenter,
+        icon:'images/map-marker-2.png'
+      });
+    marker.setMap(map);
+[{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#0c0b0b"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2f2f2"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45}]},{"featureType":"road","elementType":"labels.text.fill","stylers":[{"color":"#090909"}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#d4e4eb"},{"visibility":"on"}]},{"featureType":"water","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#fef7f7"}]},{"featureType":"water","elementType":"labels.text.fill","stylers":[{"color":"#9b7f7f"}]},{"featureType":"water","elementType":"labels.text.stroke","stylers":[{"color":"#fef7f7"}]}]
+    
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
+}
+
+//new WOW().init();
+
+
+/**
+------------------
+Pretty photo
+------------------
+*/
+// if( $('.prety-photo-heldle').length ){
+  $(".prety-photo-heldle").fancybox({
+      // Options will go here
+    });
+
+//}
+
+
+
+$('.share-btn span').on('click', function(e){
+  e.preventDefault();
+  var dataThis = $(this).attr('data-this');
+  $('#'+dataThis).toggleClass('tsm');
+});
+
+
+
+
+
+})(jQuery);
